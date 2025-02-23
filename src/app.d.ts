cf65b97f37761a4011c9dca0472dbae461e03a4a
suibari <404.not.found.address@gmail.com>
2025-02-23 15:15:12 +0900

    Lexicon定義

--- src/app.d.ts
+++ src/app.d.ts
@@ -19,6 +19,39 @@ declare global {
 			headerImage: string;
 			backgroundColor: string;
 		};
+
+		// Generated Code From Lexicon
+		export interface Record {
+			hunterName?: string
+			hunterID?: string
+			hunterRank?: string
+			selectedPlatforms?: string[]
+			selectedWeapons?: Weapon
+			playTimeRange?: number[]
+			comment?: string
+			headerImage?: BlobRef
+			backgroundColor?: string
+			createdAt: string
+			[k: string]: unknown
+		}
+
+		export interface Weapon {
+			gs?: number
+			ls?: number
+			sns?: number
+			db?: number
+			hammer?: number
+			hh?: number
+			lance?: number
+			gl?: number
+			sa?: number
+			cb?: number
+			ig?: number
+			lbg?: number
+			hbg?: number
+			bow?: number
+			[k: string]: unknown
+		}
 	}
 }
 
